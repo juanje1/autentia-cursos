cf49408ecf52a9c1e534bdd36316a43a68b219dd
juanje <jjbonitodu@msn.con>
2019-01-22 11:32:09 +0100

    SubjectRepository

---
+++ src/main/java/com/juanje/cursosboot/data/SubjectRepository.java
@@ -0,0 +1,15 @@
+package com.juanje.cursosboot.data;
+
+import com.juanje.cursosboot.domain.Subject;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.Repository;
+
+public interface SubjectRepository extends Repository<Subject, Long> {
+
+    Page<Subject> findByCourseId(Long courseId, Pageable pageable);
+
+    Page<Subject> findByCourseIdAndNameContainingAllIgnoringCase(Long courseId, String name, Pageable pageable);
+
+    Subject save(Subject subject);
+}
---
+++ src/main/java/com/juanje/cursosboot/web/CourseController.java
@@ -0,0 +1,42 @@
+package com.juanje.cursosboot.web;
+
+import com.juanje.cursosboot.domain.Course;
+import com.juanje.cursosboot.domain.Subject;
+import com.juanje.cursosboot.service.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/courses")
+@CrossOrigin
+public class CourseController {
+
+    private CourseService courseService;
+
+    @Autowired
+    public CourseController (CourseService courseService){
+        this.courseService = courseService;
+    }
+
+    @GetMapping
+    public Page<Course> findCourses(String name, Pageable pageable){
+        return courseService.findCourses(name, pageable);
+    }
+
+    @GetMapping("/{id}")
+    public Course get(@PathVariable Long id){
+        return courseService.get(id);
+    }
+
+    @GetMapping("/{id}/subjects")
+    public Page<Subject> findSubjects(@PathVariable Long id, String name, Pageable pageable){
+        return courseService.findSubjects(id, name, pageable);
+    }
+
+    @PostMapping("/{id}/subjects")
+    public Subject addSubject(@PathVariable Long id, @RequestBody Subject subject){
+        return courseService.addSubject(id, subject);
+    }
+}
